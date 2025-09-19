5969e5483122fc9098d891940dda86c55c6dc841
Adrián Vaquero Sánchez-Valladares <adrianvsv@icloud.com>
2025-09-19 18:36:57 +0200

    feat(): Estructura Inicial de la aplicación.

---
+++ TiendaUPM/src/main/java/org/upm/poo/Main.java
@@ -0,0 +1,10 @@
+package org.upm.poo;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Welcome to the ticket module App.");
+        System.out.println("Ticket module. Type 'help' to see commands.");
+    }
+}
